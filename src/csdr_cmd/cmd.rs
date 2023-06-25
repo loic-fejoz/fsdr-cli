13ac6614aa285b9492f8203610444a69af3e36c3
Loïc Fejoz <loic@fejoz.net>
2023-06-25 18:20:22 +0200

    chore(clippy): Fix lifetime

--- src/csdr_cmd/cmd.rs
+++ src/csdr_cmd/cmd.rs
@@ -4,9 +4,9 @@ use futuresdr::anyhow::{bail, Result};
 use pest::iterators::Pair;
 
 pub trait CsdrCmd<'i> {
-    fn arg<'a, const N: usize>(&self, msg: impl Into<&'static str>) -> Result<&'i str>;
-    fn type_arg<'a, const N: usize>(&self, msg: impl Into<&'static str>) -> Result<GrcItemType>;
-    fn optional_arg<'a, const N: usize>(&self) -> Result<Option<&'i str>>;
+    fn arg<const N: usize>(&self, msg: impl Into<&'static str>) -> Result<&'i str>;
+    fn type_arg<const N: usize>(&self, msg: impl Into<&'static str>) -> Result<GrcItemType>;
+    fn optional_arg<const N: usize>(&self) -> Result<Option<&'i str>>;
 }
 
 impl<'i> CsdrCmd<'i> for Pair<'i, Rule> {
